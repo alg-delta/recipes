6b101c6732d612aa24c10b89e6bd1932bf624810
alg-delta <155776375+alg-delta@users.noreply.github.com>
2026-06-05 17:13:05 +0300

    Add files via upload

---
+++ scripts/script.js
@@ -0,0 +1,242 @@
+let JB = document.querySelector('.lkk');
+let df = document.querySelector('.lp');
+let wq = document.querySelector('.pl');
+let lf = document.querySelector('.jh');
+let dk = document.querySelector('.po');
+let dp = document.querySelector('.lk');
+let closeModal = document.querySelector('.close-modal');
+let madalWrapper =document.querySelector('.modal-wrapper');
+
+let JB0 = document.querySelector('.lkk0');
+let df0 = document.querySelector('.lp0');
+let wq0 = document.querySelector('.pl0');
+let lf0 = document.querySelector('.jh0');
+let dk0= document.querySelector('.po0');
+let dp0 = document.querySelector('.lk0');
+
+JB.addEventListener('click',function(){
+	madalWrapper.style.display = 'flex';
+	JB0.style.display = 'block';
+	df0.style.display = 'none';
+	wq0.style.display = 'none';
+	lf0.style.display = 'none';
+	dk0.style.display = 'none';
+	dp0.style.display = 'none';
+});	
+
+df.addEventListener('click',function(){
+	madalWrapper.style.display = 'flex';
+	JB0.style.display = 'none';
+	df0.style.display = 'block';
+	wq0.style.display = 'none';
+	lf0.style.display = 'none';
+	dk0.style.display = 'none';
+	dp0.style.display = 'none';
+});
+wq.addEventListener('click',function(){
+	madalWrapper.style.display = 'flex';
+	JB0.style.display = 'none';
+	df0.style.display = 'none';
+	wq0.style.display = 'block';
+	lf0.style.display = 'none';
+	dk0.style.display = 'none';
+	dp0.style.display = 'none';
+});
+lf.addEventListener('click',function(){
+	madalWrapper.style.display = 'flex';
+	JB0.style.display = 'none';
+	df0.style.display = 'none';
+	wq0.style.display = 'none';
+	lf0.style.display = 'block';
+	dk0.style.display = 'none';
+	dp0.style.display = 'none';
+});
+dk.addEventListener('click',function(){
+	madalWrapper.style.display = 'flex';
+	JB0.style.display = 'none';
+	df0.style.display = 'none';
+	wq0.style.display = 'none';
+	lf0.style.display = 'none';
+	dk0.style.display = 'block';
+	dp0.style.display = 'none';
+});
+dp.addEventListener('click',function(){
+	madalWrapper.style.display = 'flex';
+	JB0.style.display = 'none';
+	df0.style.display = 'none';
+	wq0.style.display = 'none';
+	lf0.style.display = 'none';
+	dk0.style.display = 'none';
+	dp0.style.display = 'block';
+});
+closeModal.addEventListener('click',function(){
+	madalWrapper.style.display = 'none';
+});	
+	
+	madalWrapper.addEventListener('click',function(){
+	if(event.target === madalWrapper){
+	madalWrapper.style.display = 'none';
+	
+	
+	}
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
